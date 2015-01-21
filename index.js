546e1fef701ee5c5ca7d8e45cb77e3732c2ac01e
natew <natewienert@gmail.com>
2015-01-20 21:16:27 -0800

    save: Refactor

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-// runs a simple express server to serve assets
-// & a webpack-dev-server for serving the app
-// or in Build looks for a bundle and index and serves
-
-var express = require('express');
-var Path = require('path');
-var Yargs = require('yargs').argv;
-var fs = require('fs');
-var Cors = require('cors');
-
-var webpackServer = require('./lib/webpackServer');
-var makeBuildDir = require('./lib/makeBuildDir');
-
-var server = express();
-
 // opts:
 //   staticPaths: array of strings, relative paths of where to serve static assets
 //   dir: dir of where to serve app
-//   debug: turn on debugging from webpack
-//   hostname: set hostname to serve from, default 'localhost'
+//   debug: log out stuff
 
-function setupExpress(opts) {
+var express = require('express');
+var Path = require('path');
+var fs = require('fs');
+var cors = require('cors');
+
+function runServer(opts) {
+  console.log('Starting...');
+
+  var server = express();
+  var layout = getLayout(opts);
+
   server.set('port', opts.port);
-  server.use(Cors());
+  server.use(cors());
 
   var staticPaths = opts.staticPaths || [
     '/build/public',
@@ -33,76 +27,11 @@ function setupExpress(opts) {
   staticPaths.forEach(function(path) {
     server.use('/assets', express.static(opts.dir + path));
   });
-}
-
-function setupWebpackDevServer(opts, cb) {
-  opts.hostname = opts.hostname || 'localhost';
-
-  webpackServer(opts, function(template) {
-    server.get('*', function(req, res) {
-      res.send(template);
-    });
-
-    cb();
-  });
-}
-
-function setupIsomorphicServer(opts, cb) {
-  var app = require(opts.entry || opts.dir + '/build/prerender/main.js');
-  opts.stats = opts.stats || require(opts.dir + '/build/stats.json');
-
-  if (opts.debug) {
-    console.log('entry', opts.entry);
-    console.log('stats', opts.stats);
-    console.log('path', opts.path);
-    console.log();
-  }
 
   server.get('*', function(req, res) {
-    var template = renderIsomorphicApp(app, req.path, opts);
-    res.send(template);
+    res.send(layout);
   });
 
-  cb();
-}
-
-// todo: make this work
-function renderIsomorphicApp(app, path, opts) {
-  return new Promise(function(resolve, reject) {
-    if (debug)
-      console.log('request: ', path);
-
-    // run app
-    app({ location: path }, function(html, data) {
-      if (debug) {
-        console.log('ran app, received...');
-        console.log('html:');
-        console.log(html);
-        console.log('data:');
-        console.log(data);
-      }
-
-      // read ${APPDIR}/assets/index.html
-      var layout = fs.readFileSync(opts.dir + '/app/assets/layout.html').toString();
-
-      if (debug)
-        console.log('layout', layout);
-
-      // var STYLE_URL = 'main.css?' + stats.hash;
-      // var SCRIPT_URL = [].concat(stats.assetsByChunkName.main)[0] + '?' + stats.hash;
-
-      var output = layout
-        .replace('<!-- CONTENT -->', html)
-        .replace('<!-- DATA -->', '<script>window.SERVER_DATA = ' + JSON.stringify(data) + ';</script>')
-        .replace('<!-- STYLES -->', '<link rel="stylesheet" type="text/css" href="/' + opts.stats.styleUrl + '" />')
-        .replace('<!-- SCRIPTS -->', '<script src="/' + opts.stats.scriptUrl + '"></script>');
-
-      resolve(output);
-    });
-  });
-}
-
-function startServer() {
   console.log('Running express server on', server.get('port'), '...');
 
   server.listen(
@@ -110,17 +39,19 @@ function startServer() {
   );
 }
 
-// this is designed to take options from the reapp CLI,
-// but could be used outside of it as it originally was
-module.exports = function(opts) {
-  opts = opts || Yargs;
+function getLayout(opts) {
+  var hostname = opts.hostname || 'localhost';
+  var base = 'http://' + hostname + ':' + opts.wport + '/';
 
-  console.log('Starting...');
+  if (opts.debug)
+    console.log('making layout with scripts: ', opts.scripts);
 
-  // order not important
-  setupExpress(opts);
+  var scripts = opts.scripts.map(function(key) {
+    return '<script src="' + base + key + '.js"></script>';
+  });
 
-  return opts.build ?
-    setupIsomorphicServer(opts, startServer) :
-    setupWebpackDevServer(opts, startServer);
-};
+  return fs.readFileSync(opts.layout).toString()
+    .replace('<!-- SCRIPTS -->', scripts.join("\n"));
+}
+
+module.exports = runServer;
